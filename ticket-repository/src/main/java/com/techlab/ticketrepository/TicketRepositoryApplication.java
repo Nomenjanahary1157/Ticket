628549cccab3bb3129a7291ff3b1c250c693cab5
Kadmiel <ralyandrynyainakadmiel@gmail.com>
2024-08-13 10:44:08 +0300

    base project

---
+++ ticket-repository/src/main/java/com/techlab/ticketrepository/TicketRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.techlab.ticketrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketRepositoryApplication.class, args);
+    }
+
+}

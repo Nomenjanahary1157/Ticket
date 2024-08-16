812f3a1448f18ee465a554645640d5f669ef0852
Raly04 <162048893+Raly04@users.noreply.github.com>
2024-08-16 10:34:15 +0300

    rebuild base project

--- ticket-service/src/main/java/com/techlab/ticketservice/UserService.java
+++ ticket-service/src/main/java/com/techlab/ticketservice/UserService.java
@@ -13,4 +13,8 @@ public class UserService {
     public User save(User user) {
         return userRepository.save(user);
     }
+
+    public void delete(Integer id) {
+        userRepository.deleteById(id);
+    }
 }

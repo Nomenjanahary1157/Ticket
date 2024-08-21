f80c9f2ea1c01a91e79bffaa45c1b564fe7fd639
Nomenjanahary1157 <89528663+Nomenjanahary1157@users.noreply.github.com>
2024-08-21 14:34:44 +0300

    get Ticket from his status

--- ticket-webapp/src/main/java/com/techlab/ticketwebapp/controllers/UserController.java
+++ ticket-webapp/src/main/java/com/techlab/ticketwebapp/controllers/UserController.java
@@ -15,8 +15,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/api/users")
